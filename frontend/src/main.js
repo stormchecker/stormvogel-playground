a4229059c4f38b8e1d6fc65eaca784527eb6a7cf
Arthur Idema <arthur.idema@ru.nl>
2025-03-01 20:44:19 +0100

    Updated svelte, removed unnecessary dependencies and configurations, added new configs

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,8 @@
-import App from './App.svelte';
+import { mount } from 'svelte'
+import App from './App.svelte'
 
-const app = new App({
-	target: document.body,
-	props: {
-		name: 'world'
-	}
-});
+const app = mount(App, {
+  target: document.getElementById('app'),
+})
 
-export default app;
+export default app
